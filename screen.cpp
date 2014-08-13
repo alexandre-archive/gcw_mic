bec749ea2aba43c6ad918fb96d50b8490bddd0cd
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2014-08-13 00:22:15 -0300

    Add button events.

--- screen.cpp
+++ screen.cpp
@@ -1,27 +1,52 @@
-#define L_BUTTON      "tab"
-#define R_BUTTON      "backspace"
+#ifdef MIPS
+    #define L_BUTTON      "tab"
+    #define R_BUTTON      "backspace"
 
-#define X_BUTTON      "left shift"
-#define Y_BUTTON      "space"
-#define A_BUTTON      "left ctrl"
-#define B_BUTTON      "left alt"
+    #define X_BUTTON      "left shift"
+    #define Y_BUTTON      "space"
+    #define A_BUTTON      "left ctrl"
+    #define B_BUTTON      "left alt"
 
-#define SELECT_BUTTON ""
-#define START_BUTTON  ""
+    #define SELECT_BUTTON ""
+    #define START_BUTTON  ""
 
-#define UP_BUTTON     "up"
-#define DOWN_BUTTON   "down"
-#define LEFT_BUTTON   "left"
-#define RIGHT_BUTTON  "right"
+    #define UP_BUTTON     "up"
+    #define DOWN_BUTTON   "down"
+    #define LEFT_BUTTON   "left"
+    #define RIGHT_BUTTON  "right"
 
-#define PAUSE_BUTTON  "pause"
+    #define PAUSE_BUTTON  "pause"
 
-/* select + start button pressed togheter. */
-#define RETURN_BUTTON "return"
+    /* select + start button pressed togheter. */
+    #define RETURN_BUTTON "return"
+#else
+    #define L_BUTTON      SDLK_l
+    #define R_BUTTON      SDLK_r
+
+    #define X_BUTTON      SDLK_x
+    #define Y_BUTTON      SDLK_y
+    #define A_BUTTON      SDLK_a
+    #define B_BUTTON      SDLK_b
+
+    #define SELECT_BUTTON SDLK_ESCAPE
+    #define START_BUTTON  ""
+
+    #define UP_BUTTON     "up"
+    #define DOWN_BUTTON   "down"
+    #define LEFT_BUTTON   "left"
+    #define RIGHT_BUTTON  "right"
+
+    #define PAUSE_BUTTON  "pause"
+
+    /* select + start button pressed togheter. */
+    #define RETURN_BUTTON "return"
+#endif
+
 
 #include <SDL.h>
 #include <SDL_image.h>
 #include <string>
+#include <iostream>
 
 using namespace std;
 
@@ -29,88 +54,25 @@ using namespace std;
 #define HEIGHT 240
 #define DEPTH  8
 
-SDL_Surface *screen;
+SDL_Surface *screen,
+            *rec_btn,
+            *stop_btn,
+            *play_btn,
+            *pause_btn;
 
-void main_loop()
-{
+bool is_recording = false,
+     is_playing = false;
 
-    SDL_Event event;
-    bool quit = false;
 
-    while(!quit)
-    {
-        while(SDL_PollEvent(&event))
-        {
-            switch(event.type)
-            {
-                /* Keyboard event */
-                /* Pass the event data onto PrintKeyInfo() */
-                //case SDL_KEYDOWN:
-                case SDL_KEYUP:
-                    //PrintKeyInfo(&event.key);
-                    //SDLKey keyPressed = event.key.keysym.sym;
+void apply_surface(SDL_Surface *image, int x, int y);
+void apply_surface(SDL_Surface *image, int x, int y, int w, int h);
+void draw_rec_stop();
+void draw_play_pause();
+SDL_Surface *load_png(string path);
+void load_resources();
+void main_loop();
 
-                    switch (event.key.keysym.sym/*keyPressed*/)
-                    {
-                        case SDLK_ESCAPE:
-                            quit = true;
-                        break;
-                        default:
-                        break;
-                    }
-                break;
-                case SDL_QUIT: /* window close */
-                    quit = true;
-                break;
-                default:
-                break;
-            }
-        }
-    }
-}
-/*
-SDL_Surface* loadSurface( string path )
-{
-    //The final optimized image 
-    SDL_Surface* optimizedSurface = NULL;
-    //Load image at specified path
-    SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
-
-    if( loadedSurface == NULL )
-    {
-        printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
-    }
-    else
-    {
-        //Convert surface to screen format 
-        optimizedSurface = SDL_ConvertSurface( loadedSurface, screen->format, NULL );
-
-        if( optimizedSurface == NULL )
-        {
-            printf( "Unable to optimize image %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
-        }
-
-        //Get rid of old loaded surface 
-
-        SDL_FreeSurface( loadedSurface );
-    }
-
-    return optimizedSurface;
-}
-*/
-SDL_Surface* load_png(string path)
-{
-    SDL_Surface *image = IMG_Load(path.c_str());
-
-    if(!image)
-    {
-        printf("IMG_Load: %s\n", IMG_GetError());
-    }
-
-    return image;
-}
-
-void apply_surface(SDL_Surface* image, int x, int y)
+void apply_surface(SDL_Surface *image, int x, int y)
 {
     SDL_Rect tmp;
 
@@ -125,6 +87,94 @@ void apply_surface(SDL_Surface* image, int x, int y)
 
     SDL_Flip(screen);
 }
+
+void apply_surface(SDL_Surface *image, int x, int y, int w, int h)
+{
+    Uint32 color = SDL_MapRGB(screen->format, 0, 0, 0);
+
+    SDL_Rect tmp;
+    tmp.x = x;
+    tmp.y = y;
+    tmp.w = w;
+    tmp.h = h;
+
+    SDL_FillRect(screen, &tmp, color);
+
+    apply_surface(image, tmp.x, tmp.y);
+}
+
+void draw_rec_stop()
+{
+    SDL_Surface *source = is_recording ? stop_btn : rec_btn;
+    apply_surface(source, 126, 120, 32, 32);
+}
+
+void draw_play_pause()
+{
+    SDL_Surface *source = is_playing ? pause_btn : play_btn;
+    apply_surface(source, 162, 120, 32, 32);
+}
+
+SDL_Surface *load_png(string path)
+{
+    SDL_Surface *image = IMG_Load(path.c_str());
+
+    if(!image)
+    {
+        printf("IMG_Load: %s\n", IMG_GetError());
+    }
+
+    return image;
+}
+
+void load_resources()
+{
+    rec_btn = load_png("resources/32/player_record.png");
+    stop_btn = load_png("resources/32/player_stop.png");
+    play_btn = load_png("resources/32/player_play.png");
+    pause_btn = load_png("resources/32/player_pause.png");
+}
+
+void main_loop()
+{
+    SDL_Event event;
+    bool quit = false;
+
+    while(!quit && SDL_WaitEvent(&event))
+    {
+        switch(event.type)
+        {
+            case SDL_KEYDOWN:
+            //case SDL_KEYUP:
+            {
+                SDLKey key = event.key.keysym.sym;
+                switch (key)
+                {
+                    case SELECT_BUTTON:
+                        quit = true;
+                    break;
+                    case A_BUTTON:
+                        is_recording = !is_recording;
+                        draw_rec_stop();
+                    break;
+                    case B_BUTTON:
+                        is_playing = !is_playing;
+                        draw_play_pause();
+                    break;
+                    default:
+                    break;
+                }
+            }
+            break;
+            case SDL_QUIT: /* window close */
+                quit = true;
+            break;
+            default:
+            break;
+        }
+    }
+}
+
 
 int main()
 {
@@ -151,13 +201,10 @@ int main()
         SDL_Quit();
     }
 
-    SDL_Surface* rec_btn = load_png("resources/32/player_record.png");
-    SDL_Surface* stop_btn = load_png("resources/32/player_stop.png");
-    SDL_Surface* play_btn = load_png("resources/32/player_play.png");
-    SDL_Surface* pause_btn = load_png("resources/32/player_pause.png");
+    load_resources();
 
-    apply_surface(rec_btn, 126, 120);
-    apply_surface(play_btn, 162, 120);
+    draw_rec_stop();
+    draw_play_pause();
 
 /*
 SDL_Rect r = {0, 0, 10, 10};
